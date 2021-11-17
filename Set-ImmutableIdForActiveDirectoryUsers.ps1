040f694f124b229f4db37a3dc69aff473efafd1f
Jagdeep Singh <jagdeep.singh@thalesgroup.com>
2021-11-17 18:42:20 -0500

    Updated script to retrun queryUrl

--- Set-ImmutableIdForActiveDirectoryUsers.ps1
+++ Set-ImmutableIdForActiveDirectoryUsers.ps1
@@ -1,1 +1,13 @@
-﻿write-output "YES ITS WORKING"
+﻿
+param
+(
+    [Parameter (Mandatory = $false)]
+    [object]
+    $webhookData
+)
+
+$webhookBodyObject = ConvertFrom-JSON -InputObject $webhookData.RequestBody
+
+Invoke-RestMethod -Method post -Uri $webhookBodyObject.CallBackUrl
+
+write-output "YES ITS WORKING"
